12c3b8ae719fed6d3aeed05082f3684d374bd8d1
Rachit Bhatia <rbha0031@student.monash.edu>
2023-04-11 02:06:49 +0800

    changed token from image to swing component drawing

--- Sprint2/Token.java
+++ Sprint2/Token.java
@@ -5,26 +5,31 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 
-public class Token extends JLabel {
+public class Token extends JComponent {
 
     private int xCoordinate;
     private int yCoordinate;
+    private final int radius = 37;
 
     public Token(int x, int y){
 
         xCoordinate = x;
         yCoordinate = y;
         setBounds(xCoordinate,yCoordinate,50,50);
-        ImageIcon image = new ImageIcon(new ImageIcon("/Users/rachit/Documents/Monash/Y2S2/FIT3077/project/Sprint2/Circle_-_black_simple_fullpage.svg.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
-        setIcon(image);
 
         ModMouseAdapter mouseActionRecorder = new ModMouseAdapter(this, xCoordinate,yCoordinate);
         this.addMouseListener(mouseActionRecorder);
         this.addMouseMotionListener(mouseActionRecorder);
+        this.setPreferredSize(new Dimension(50, 50));
     }
 
-
-
-
+    //used for creating a filled oval
+    @Override
+    public void paintComponent(Graphics tokenShape) {
+        super.paintComponent(tokenShape);
+        tokenShape.setColor(Color.WHITE);
+        tokenShape.fillOval(xCoordinate, yCoordinate, 30, 30);  //fill token shape with white colour
+        this.setBorder(BorderFactory.createLineBorder(Color.black, 2));  //rectangular border around the token component
+    }
 
 }
