a40db204bb943702327b6121502ce253a4c39cf8
Rachit Bhatia <rbha0031@student.monash.edu>
2023-04-11 11:46:52 +0800

    added all 18 tokens and restructured the code

--- Sprint2/MainDriver.java
+++ Sprint2/MainDriver.java
@@ -2,6 +2,7 @@ package project.Sprint2;
 
 import javax.swing.*;
 import java.awt.*;
+import project.Sprint2.Token;
 
 public class MainDriver {
 
@@ -15,25 +16,29 @@ public class MainDriver {
         mainWindow.setLocation(0,0);
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //default closing action
 
-        JPanel panel = new JPanel();
-        panel.setLayout(null);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(Color.GRAY);
+        mainPanel.setLayout(null);
+        mainPanel.setPreferredSize(new Dimension(screenDimension.width, screenDimension.height));
+
+        //Adding both player's Tokens to the Panel containt
+        int tokenSpacing = 0;
+        for (int i = 1; i <= 9; i++){
+            mainPanel.add(new Token(screenDimension.width/8,screenDimension.height/8 + tokenSpacing, Color.BLACK));
+            mainPanel.add(new Token(screenDimension.width - screenDimension.width/7,screenDimension.height/8 + tokenSpacing, Color.WHITE));
+            tokenSpacing += 70;
+        }
 
         // Intersection Points
         IntersectionPoint intersectionPoint1 = new IntersectionPoint(50,50,20,20);
         IntersectionPoint intersectionPoint2 = new IntersectionPoint(200,50,20,20);
 
-        // Tokens
-        Token token = new Token(100,100);
-
-        // Adding tokens and intersection points to the panel container
-        panel.add(token);
-        panel.add(intersectionPoint1);
-        panel.add(intersectionPoint2);
-
-        panel.setBackground(Color.GRAY);   //setting background colour of application
+//      Adding intersection points to the panel container
+        mainPanel.add(intersectionPoint1);
+        mainPanel.add(intersectionPoint2);
 
         // Adding the panel container that has all the tokens and intersection points to the frame
-        mainWindow.add(panel,BorderLayout.CENTER);
+        mainWindow.getContentPane().add(mainPanel);
 
         mainWindow.setVisible(true);    //application window visibility
     }
