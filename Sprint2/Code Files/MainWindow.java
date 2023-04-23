2425ce76421590033714a2fd8992a25f7436f974
Rachit Bhatia <rbha0031@student.monash.edu>
2023-04-23 19:25:12 +0800

    refactored driver code into separate window class

---
+++ Sprint2/Code Files/MainWindow.java
@@ -0,0 +1,54 @@
+package project.Sprint2;
+
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class MainWindow {
+
+    private static JFrame mainWindow = new JFrame("9 Men's Morris");  //initialising the main application window;
+    private static Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();  //accessing the dimensions of the screen;
+    private static JPanel mainPanel = new JPanel();    //instantiating the panel for the main window display;
+
+
+    public static void setupWindow(){
+        /**
+         * Setting the window size and preferences
+         */
+        //setting the dimensions of the main window frame based on the screen size
+        mainWindow.setSize(screenDimension.width, screenDimension.height);  //window size is of the screen size
+        mainWindow.setLocation(0,0);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //default closing action
+
+        //setting preferences for the main window panel
+        mainPanel.setBackground(Color.GRAY);
+        mainPanel.setLayout(null);
+        mainPanel.setPreferredSize(new Dimension(screenDimension.width, screenDimension.height));
+    }
+
+
+    public static void addAllItems(){
+        /**
+         * Creation and addition of the all items (tokens, intersections, game board) into the game
+         */
+
+        //Adding both player's Tokens to the Panel container
+        int tokenSpacing = 0;
+        for (int i = 1; i <= 9; i++){
+            mainPanel.add(new Token(screenDimension.width/8,screenDimension.height/8 + tokenSpacing, Color.BLACK));
+            mainPanel.add(new Token(screenDimension.width - screenDimension.width/7,screenDimension.height/8 + tokenSpacing, Color.WHITE));
+            tokenSpacing += 70;
+        }
+
+        //creation and addition of game board into the main window panel
+        GameBoard gameBoard = GameBoard.getInstance();
+        gameBoard.setDimensionsOfBoard(screenDimension);
+        gameBoard.addIntersections(gameBoard.getWidth(),gameBoard.getHeight());  //adding intersection points
+        mainPanel.add(gameBoard);
+
+        // Adding the panel container that has all the tokens and intersection points to the frame
+        mainWindow.getContentPane().add(mainPanel);
+        mainWindow.setVisible(true);    //application window visibility
+    }
+}
