1d50fcae0a57b3d16be01cdf57e14b21181c2020
Rachit Bhatia <rbha0031@student.monash.edu>
2023-04-23 19:27:00 +0800

    driver class updated to contain minimal code

---
+++ Sprint2/Code Files/ApplicationDriver.java
@@ -0,0 +1,9 @@
+package project.Sprint2;
+
+public class ApplicationDriver {
+
+    public static void main(String[] args) {
+        MainWindow.setupWindow();
+        MainWindow.addAllItems();
+    }
+}
