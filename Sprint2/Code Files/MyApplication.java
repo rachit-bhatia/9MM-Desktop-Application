bdfa9ba7679419cfa81b6663c3dff55b0b876fe6
Rachit Bhatia <rbha0031@student.monash.edu>
2023-04-23 15:23:43 +0800

    moved all java classes to new folder

---
+++ Sprint2/Code Files/MyApplication.java
@@ -0,0 +1,37 @@
+package project.Sprint2;
+import javax.swing.*;
+import java.awt.*;
+
+public class MyApplication extends JFrame {
+
+    public MyApplication() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(Color.GRAY);
+        mainPanel.setPreferredSize(new Dimension(400, 400));
+
+        OvalComponent oval = new OvalComponent();
+        mainPanel.add(oval);
+
+        this.add(mainPanel);
+        this.pack(); // adjust frame size based on the preferred size of its components
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MyApplication();
+    }
+
+    private static class OvalComponent extends JComponent {
+
+        @Override
+        public void paintComponent(Graphics token) {
+            token.setColor(Color.BLUE);
+            token.fillOval(50, 50, 100, 100);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(200, 200);
+        }
+    }
+}
