63795efea4d4bf94a6299e139573bdb66317a806
Rachit Bhatia <rbha0031@student.monash.edu>
2023-04-27 15:50:48 +0800

    added steps to run application in documentation

--- Sprint2/Code Files/src/game/ApplicationDriver.java
+++ Sprint2/Code Files/src/game/ApplicationDriver.java
@@ -1,12 +1,12 @@
 package game;
 
 /**
- * Class Application Driver.
- * <p>
- * Created  by Rachit Bhatia
+ * Steps to run and test the application:
+ * - The Application can be run by launching the jar file (9MM_DevDynasty.jar)
+ * - In order to be able to run the jar file, the device should have at least Java Version 17.0.2 installed. Newer versions will also work.
+ * - To install the correct JDK using IntelliJ Idea IDE, navigate to Project Structure in the File menu and choose SDK 17 under Project
  *
- * @author Rachit Bhatia
- * Modified by: -
+ * - If there are any issues faced while launching 9MM_DevDynasty.jar, or it cannot be run, the 9MM application can still be launched by simply running this file i.e. the ApplicationDriver class file.
  */
 public class ApplicationDriver {
 
