a4e851b205d1bfa08bfb8200e5fd45bbc59cb601
Rachit Bhatia <rbha0031@student.monash.edu>
2023-05-13 11:19:31 +0800

    shifted change state function to MillChecker and implemented actions to remove token

--- Sprint2/Code Files/src/game/RemoveMove.java
+++ Sprint2/Code Files/src/game/RemoveMove.java
@@ -1,29 +1,55 @@
 package game;
 
-import java.awt.*;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class RemoveMove extends MouseAdapter {
 
+    /**
+     * token to be removed
+     */
+    private Token tokenInstance;
+
     public RemoveMove(Token tokenInstance) {
+        this.tokenInstance = tokenInstance;
     }
 
-    public static void highlightRemovableTokens(Player curPlayer){
-        Player oppPlayer;
-        if (curPlayer == Game.getInstance().getPlayer1()){
-            oppPlayer = Game.getInstance().getPlayer2();
-        }
-        else {
-            oppPlayer = Game.getInstance().getPlayer1();
-        }
+    /**
+     * Resets the remaining tokens and board to their original appearance and removed the selected token from the game
+     * @param cursor the mouse pointer
+     */
+    @Override
+    public void mouseClicked(MouseEvent cursor) {
 
-        for (Token token : oppPlayer.getTokenList()){
-            for (Component boardItem: GameBoard.getInstance().getComponents()){
-                if (boardItem.equals(token)){
-                    token.setToRemove(true);
-                    token.repaint();
-                }
+        //resetting token appearance to remove red highlight of border after selection has been made
+        for (IntersectionPoint position : GameBoard.getInstance().getIntersectionPoints()){
+            Token curToken = position.getTokenInstance();
+            if (curToken != null){
+                curToken.setToRemove(false);  //removal state false
+                curToken.repaint();
             }
         }
+
+        GameBoard.getInstance().remove(tokenInstance);  //removing token from game board
+        tokenInstance.getIntersectionPoint().removeToken();  //removing token from intersection point
+        tokenInstance.getPlayer().getTokenList().remove(tokenInstance); //removing token from player's list
+
+        //updating the UI state of the board
+        GameBoard.getInstance().revalidate();
+        GameBoard.getInstance().repaint();
+
+        Game.getInstance().incrementTurn(); //turn increment only after token has been removed
     }
 }
+
+
+//FIXME: DELETE LATER!!
+//                if (position.getTokenInstance().getPlayer().getCurrentStateOfMove() == CurrentStateofMove.PLACING) {
+//                    position.getTokenInstance().changeListener(null);
+//                }
+//                else if (position.getTokenInstance().getPlayer().getCurrentStateOfMove() == CurrentStateofMove.SLIDING) {
+//                    position.getTokenInstance().changeListener(new SlidingMove(position.getTokenInstance(), position.getTokenInstance().getX(), position.getTokenInstance().getY()));
+//                }
+//                else{
+//                    position.getTokenInstance().changeListener(new FlyingMove(position.getTokenInstance(), position.getTokenInstance().getX(), position.getTokenInstance().getY()));
+//                }
