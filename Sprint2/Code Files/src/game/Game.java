c538e033e2e9632ac5f6faa5e68dc3d363f73afc
TanJunYu <jtan0245@student.monash.edu>
2023-05-14 14:45:49 +0800

    No valid moves makes the opponent wins the game

--- Sprint2/Code Files/src/game/Game.java
+++ Sprint2/Code Files/src/game/Game.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Game implements NeighbourPositionFinder{
 
@@ -35,11 +36,21 @@ public class Game implements NeighbourPositionFinder{
         Border playerIdentifier = BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.GREEN.brighter().brighter(), Color.GREEN.darker().darker());
         Border identifierThickness = BorderFactory.createEmptyBorder(1,1,1,1);
 
+
+        boolean player1NoValidMove = false;
+        boolean player2NoValidMove = false;
         // While both players still have 3 or more tokens, keep the game running
         while (player1.getNumberOfTokens() >= 3 && player2.getNumberOfTokens() >= 3){
 
             // update state of move if needed
             player1.updateStateOfMove();
+
+            if (player1.getCurrentStateOfMove() == CurrentStateofMove.SLIDING){
+                player1NoValidMove = checkIfPlayerHasValidSlidingMove(player1);
+                if (player1NoValidMove){
+                    break;
+                }
+            }
 
 
             // While a valid move is not yet made by the player 1
@@ -54,8 +65,17 @@ public class Game implements NeighbourPositionFinder{
             // After player 1 turn is finished
             player1.setPlayerTurn(false);
 
+
             // update state of move if needed
             player2.updateStateOfMove();
+
+            if (player2.getCurrentStateOfMove() == CurrentStateofMove.SLIDING){
+                player2NoValidMove = checkIfPlayerHasValidSlidingMove(player2);
+                if (player2NoValidMove){
+                    break;
+                }
+            }
+
 
             // While a valid move is not yet made by the player 2
             while ( this.turn % 2 == 1 && player2.getNumberOfTokens() >= 3){
@@ -71,10 +91,10 @@ public class Game implements NeighbourPositionFinder{
 
         }
 
-        if (player1.getTokenList().size() < 3){
+        if (player1.getTokenList().size() < 3 || player1NoValidMove){
             endMessage += "Player 2!\nYou win the game!";
         }
-        else if (player2.getTokenList().size() < 3){
+        else if (player2.getTokenList().size() < 3 || player2NoValidMove){
             endMessage += "Player 1!\nYou win the game!";
         }
 
@@ -82,7 +102,30 @@ public class Game implements NeighbourPositionFinder{
 
     }
 
+    public boolean checkIfPlayerHasValidSlidingMove(Player player){
+        ArrayList<Token> tokens = player.getTokenList();
 
+        boolean noValidMoves = true;
+        for (Token token : tokens){
+            if (token.isTokenPlaced()){
+                IntersectionPoint intersectionPoint = token.getIntersectionPoint();
+
+                for ( IntersectionPoint neighbour : findNeighbouringIntersections(intersectionPoint)){
+                    if (!neighbour.hasToken()){
+                        noValidMoves = false;
+                        break;
+                    }
+                }
+
+                if (!noValidMoves){
+                    break;
+                }
+
+            }
+        }
+
+        return noValidMoves;
+    }
     public Player getPlayer1() {
         return player1;
     }
